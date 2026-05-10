2d3ab871dea7b3b20a3be47f7d1b577f7da0f5c5
ricardomlee <ricardomlee@users.noreply.github.com>
2026-05-10 23:43:55 +0800

    feat: flatten post URLs to root, remove /posts prefix from slugs

--- src/utils/getPath.ts
+++ src/utils/getPath.ts
@@ -21,7 +21,7 @@ export function getPath(
     .slice(0, -1) // remove the last segment (file name)
     .map(segment => slugifyStr(segment)); // slugify
 
-  const basePath = includeBase ? "/posts" : "";
+  const basePath = includeBase ? "" : "";
 
   // Making sure `id` does not contain the directory
   const blogId = id.split("/");
